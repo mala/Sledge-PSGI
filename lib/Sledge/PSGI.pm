84b0f22da11950f026f03e079d2d9a4a692dbc55
mala <github@ma.la>
2009-10-17 17:03:32 +0900

    initial commit

---
+++ lib/Sledge/PSGI.pm
@@ -0,0 +1,33 @@
+package Sledge::PSGI;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Sledge::PSGI -
+
+=head1 SYNOPSIS
+
+  use Sledge::PSGI;
+
+=head1 DESCRIPTION
+
+Sledge::PSGI is
+
+=head1 AUTHOR
+
+mala E<lt>cpan@ma.laE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
